b9ef9ea357c0ffd5006ff37cc04dffe38528c8c4
Robert Hawkins <hawkrobe@gmail.com>
2020-12-21 18:35:39 -0800

    re-run BDA

--- modeling/run_AIS.sh
+++ modeling/run_AIS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash#
-# parallel --bar --colsep ',' "sh ./run_AIS.sh {1}" :::: input/BF_grid.csv > "output/AIS2.txt"
-~/.webppl/webppl BDA.wppl --require ./refModule/ --require webppl-csv -- --model $1 --AIS true
+# parallel --bar --colsep ',' "sh ./run_AIS.sh {1} {2}" :::: input/BF_grid.csv 
+webppl BDA.wppl --require ./refModule/ --require webppl-csv -- --chainNum $1 --model $2 --AIS true
